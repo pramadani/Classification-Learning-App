f5c1d6f9a9f737e40480556d6df000ded8e7bd58
Diksa <madediksapitra@gmail.com>
2024-06-26 15:57:11 +0700

    fix logo

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-logo = st.logo("resources/logo_crop.png")
+st.set_page_config(page_title="Classification App",
+    page_icon="🔥",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "## Developed by  \n" + "Ararya Pramadani Alief Rahman  \n" + "Azarya Santoso  \n" + "Made Diksa Pitra  \n"
+    })
+
+st.logo("resources/logo_large.png", icon_image="resources/logo_crop.png")
+
+st.image("resources/logo_crop.png", width=150)
+
+st.write("\n")
+st.write("\n")
+st.write("\n")
 
 dashboard = st.Page(
     "dashboard.py", 
